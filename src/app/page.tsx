83ff28e2cd0f28b774a2e5a8db8e80cc5d0b26b7
Deepak Kumar <kumardk8780@gmail.com>
2024-03-04 13:43:15 +0530

    Navbar Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
- <div>
-  <h1 className="text-3xl text-center mt-8">Music</h1>
- </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-3xl text-center">Music</h1>
+    </main>
   );
 }
